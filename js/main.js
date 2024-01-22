57de76822d2a51786ae5bdc30de01d604d1f0ca9
bonvi83 <bonvi83@gmail.com>
2024-01-22 19:36:58 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,73 +1,222 @@
-// DA CORREZIONE TEACHER SUPER COPIA ED INCOLLA, CHE OVVIAMENTE NON SERVE A NULLA!!!
+//### Variabili globali
+let bombElements = [];
+const bombs = createBombElements(16, 100) //min, max
+let score = 0;
+let numberOfCells = 100;
 
-const gameboardElement = document.getElementById(`gameboard`);
-const playButton = document.getElementById(`play`);
-generateGrid(gameboardElement);
+//### Richiamo le funzioni
 
+// richiamo l'elemento html container della griglia
+const gridContainer = document.getElementById("container");
+const button = document.getElementById("button");
 
-playButton.addEventListener(`click`, function () {
-    generateGrid(gameboardElement);
+createGrid(gridContainer); //la funzione createGrid(container) è stata popolata con la const gridContainer
+
+// Aggiungo la funzione all'evento sul bottone
+button.addEventListener("click", function() {
+  createGrid(gridContainer);
+  isGameOver = false; //alla pressione del testo play isGameOver torna false
 });
 
 
-function generateGrid(container) {
-    container.innerHTML = ``;
+/* =============================================== 
 
-    for (let i = 0; i < 100; i++) {
-        const cellElement = generateCell();
-        container.append(cellElement);
-    }
-}
+  ### FUNZIONI
 
-function generateCell() {
-    const cell = document.createElement(`div`);
+================================================= */
 
-    cell.classList.add(`box`);
-    cell.addEventListener(`click`, function () {
-        this.classList.toggle(`toggle`);
-    })
-    return cell;
-
-//  DA SISTEMARE: CREARE NUMERI SULLE CELLE 
-//  (E NON FUNZIONA IL TASTO PLAY)
-
-
-
-// da esercitazione collettiva di classe (servita a nulla per me!)
-
-function generateBombList(bombs, numberOfCell) {
-    while (bombListi.lenght < bombs) {
-    const randomInt = getRandomNumber(0, numberOfCell - 1);
-    if (!bombList.includes(randomInt)) {
-        bombList.push(randomInt);
-    }}
-    return bombList;
-}
-
+//### Funzione numero random
+//funzione che contiene il Math.floor(Math.random())
+//genero un numero casuale tra min e max, quindi da 1 a 100
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() + ((max + 1) - min) + min);
-}}
+
+  // return Math.floor((Math.random() * 100) + 1);
+  return Math.floor(Math.random() * ((max + 1) - min) + min); 
+  // max - min è la sottrazione che mi da l'intervallo da 1 a 100. 
+  // + 1 a max include max nell'intervallo. 
+  // + min finale include min nell'intervallo
+  
+  }
+  
+//===================================================
+
+//### Funzione per generare lista bombe
 
 
-// per generare bombe
+function createBombElements(bombs, numberOfCells) {
+  //inizializzo a vuoto un array
 
-const bombs = generateBombList(16, 100);
-
-
-cell.addEventListener(`click`, function () {
-    if (!bombs.includes(i)) {
-        this.classList.add(`clicked`);
-    } else {
-        this.classList.add(`clicked-bomb`);
+    //while (bombElements.length < 16)
+    while (bombElements.length < bombs) {
+      
+      const randomInt = getRandomNumber(1, numberOfCells); //quindi da 1 a 100
+      //includes si può leggere "se è gia incluso"
+      if (!bombElements.includes(randomInt)) {
+        //push aggiunge un nuovo elemento all'array
+        bombElements.push(randomInt);
+      }
     }
-});
+  
+    return bombElements;
+    
+}
+
+//===================================================
+
+//### Funzione "genera-cella"
+/**
+ * @param {index} text text = i
+ * @param {number} bombsEl variabile bombElements
+*/
+
+function createBox(text, bombsEl) {
+  //Crea elemento div
+  let box = document.createElement("div");
+
+  //Assegna la classe "box"
+  box.classList.add("box");
+
+  //Aggiungi il numero, prima usavo uno span, ora è il numero direttamente
+  box.innerText = text;
+
+  //*Toggle sulla cella
+  box.addEventListener("click", function() {
+    // se il gioco non è finito
+    if(!isGameOver) {
+      if(!bombsEl.includes(text)) {
+        box.classList.toggle("clicked");
+    // this.classList.toggle("clicked");
+        score++
+        if(score == numberOfCells - bombElements.length){
+          alert("HAI VINTO! Il tuo punteggio è " + score)
+          isGameOver = true;
+          score = 0;
+        }
+
+      } else {
+        box.classList.add("bomb-found");
+        alert("GAME OVER! Il tuo punteggio è " + score);
+        isGameOver = true;
+        score = 0;
+      }
+      
+    }
+  });
+
+  return box;
+}
+
+let isGameOver = false;
+
+//===================================================
+
+//### Funzione "genera-griglia"
+function createGrid(container) {
+  container.innerHTML = ''; //questa mi fa svuotare il container perché altrimenti le griglie da 100 si aggiungono, una sotto l'altra
+
+  for (let i = 1; i <= 100; i++) {
+    
+    let grid = createBox(i, bombElements); //ha richiamato la funzione sopra che fa tutte le sue operazioni e poi ritorna box. Quindi box ora viene inserita in grid 100 volte. I parametri di createBox ora sono popolati
+
+    //ora la costante grid deve essere inserita nel container
+    container.append(grid);
+
+  }
+}
 
 
 
-// NON TROVO LA QUADRA PER FAR FUNZIONARE IL TUTTO 
 
 
 
 
 
-// MEGLIO CHE DEDICHE DEL TEMPO AL RIPASSO DELLE BASI DI JS E ALLE FUNZIONI!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // DA CORREZIONE TEACHER SUPER COPIA ED INCOLLA, CHE OVVIAMENTE NON SERVE A NULLA!!!
+
+// const gameboardElement = document.getElementById(`gameboard`);
+// const playButton = document.getElementById(`play`);
+// generateGrid(gameboardElement);
+
+
+// playButton.addEventListener(`click`, function () {
+//     generateGrid(gameboardElement);
+// });
+
+
+// function generateGrid(container) {
+//     container.innerHTML = ``;
+
+//     for (let i = 0; i < 100; i++) {
+//         const cellElement = generateCell();
+//         container.append(cellElement);
+//     }
+// }
+
+// function generateCell() {
+//     const cell = document.createElement(`div`);
+
+//     cell.classList.add(`box`);
+//     cell.addEventListener(`click`, function () {
+//         this.classList.toggle(`toggle`);
+//     })
+//     return cell;
+
+// //  DA SISTEMARE: CREARE NUMERI SULLE CELLE 
+// //  (E NON FUNZIONA IL TASTO PLAY)
+
+
+
+// // da esercitazione collettiva di classe (servita a nulla per me!)
+
+// function generateBombList(bombs, numberOfCell) {
+//     while (bombListi.lenght < bombs) {
+//     const randomInt = getRandomNumber(0, numberOfCell - 1);
+//     if (!bombList.includes(randomInt)) {
+//         bombList.push(randomInt);
+//     }}
+//     return bombList;
+// }
+
+// function getRandomNumber(min, max) {
+//     return Math.floor(Math.random() + ((max + 1) - min) + min);
+// }}
+
+
+// // per generare bombe
+
+// const bombs = generateBombList(16, 100);
+
+
+// cell.addEventListener(`click`, function () {
+//     if (!bombs.includes(i)) {
+//         this.classList.add(`clicked`);
+//     } else {
+//         this.classList.add(`clicked-bomb`);
+//     }
+// });
+
+
+
+// // NON TROVO LA QUADRA PER FAR FUNZIONARE IL TUTTO 
+
+
+
+
+
+// // MEGLIO CHE DEDICHE DEL TEMPO AL RIPASSO DELLE BASI DI JS E ALLE FUNZIONI!!!
